a75fd9d3cc8642f40598f9dde108ee0ae77c292c
Divyanshu Jain <divyanshu.jain@neudesic.com>
2024-01-02 17:23:47 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,18 +1,12 @@
-// Create a web server
-// Create a route to /comments that returns the following HTML
-// <ul>
-//   <li>Comment 1</li>
-//   <li>Comment 2</li>
-//   <li>Comment 3</li>
-// </ul>
-// Run the web server and make sure it returns the HTML in the browser
+// Create web server
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
-var http = require('http');
-
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<ul><li>Comment 1</li><li>Comment 2</li><li>Comment 3</li></ul>');
-});
-
-server.listen(8080);
-console.log('Server listening on port 8080');
+// Create server at port 3000
+var server = app.listen(3000, function () {
+  console.log('Server running at http://localhost:' + server.address().port);
+}
+);
